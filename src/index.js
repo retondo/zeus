0cfd2ef1155e6c863293d543bf474bd3b5a616ae
Renan <renanmiguel93@hotmail.com>
2020-10-12 12:10:16 -0300

    :bug: Show the right port that server is listen to

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const server = require('./api/server')
 
-server.listen(process.env.PORT || 3000, error => {
+const port = process.env.PORT || 3000
+
+server.listen(port, error => {
   if (error) {
     console.error(error)
     process.exit(1)
   }
-  console.log('\u{1F3C3} API running at port 3000')
+  console.log(`\u{1F3C3} API running at port ${port}`)
 })
