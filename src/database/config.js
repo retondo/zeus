feb048d90c280ef4685d553c0964bf4a7b848caf
Renan <renanmiguel93@hotmail.com>
2020-10-12 18:55:24 -0300

    :green_heart: Migrate on Heroku

--- src/database/config.js
+++ src/database/config.js
@@ -1,10 +1,22 @@
 module.exports = {
-  dialect: 'postgres',
-  database: 'zeus',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'test',
-  define: {
-    underscored: true
+  development: {
+    dialect: 'postgres',
+    database: 'zeus',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'test',
+    define: {
+      underscored: true
+    }
+  },
+  production: {
+    username: 'rgqjvwpbujcujh',
+    password: 'ef62e266a794e8e2e956f4e54332db3064e5d5a8a946273ebbb1588fa2d26da3',
+    database: 'd889oesel9ouhk',
+    host: 'ec2-50-17-197-184.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    defines: {
+      underscored: true
+    }
   }
 }
