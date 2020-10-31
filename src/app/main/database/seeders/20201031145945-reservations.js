47ae2a7f5beaa871ba4ebd60b81dec7e42f25837
Renan <renanmiguel93@hotmail.com>
2020-10-31 12:07:33 -0300

    :card_file_box: Database seeds

---
+++ src/app/main/database/seeders/20201031145945-reservations.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const now = new Date()
+
+    await queryInterface.bulkInsert('reservations', [{
+      id: '1dc3fdc0-88ab-4290-b53f-c2889583f820',
+      account_id: '8f10d794-296b-46ef-b7cf-2603845478a4',
+      user_id: 'bc02d0b9-f177-4d4f-a0d1-114a3e8bff7c',
+      event_id: '1dc3fdc0-88ab-4290-b53f-c2889583f820',
+      approved_by: null,
+      approved_at: null,
+      created_at: now,
+      updated_at: now
+    }], { underscored: true });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('reservations', null, { underscored: true });
+  }
+};
