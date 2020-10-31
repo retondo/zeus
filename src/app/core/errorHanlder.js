783ed1028da90dfc67159c8e0bd117e8a180eb83
Renan <renanmiguel93@hotmail.com>
2020-10-31 18:39:01 -0300

    :recycle: Errors body message improved

--- src/app/core/errorHanlder.js
+++ src/app/core/errorHanlder.js
@@ -1,15 +1,28 @@
+const sequelize = require('sequelize')
+
 class ErrorHandler {
   static responseWithError(res, err) {
     if (
       err.name === 'SequelizeUniqueConstraintError' ||
       err.name === 'SequelizeValidationError'
     ) {
-      return res.status(422).send({
-        errors: err.errors
-      })
+      let errors = []
+      if (err.errors) {
+        errors = err.errors.map(err => new Object({
+          code: err.type,
+          message: err.message,
+          property: err.path
+        }))
+      }
+      return res.status(422).send({ errors })
     }
 
-    return res.status(400).send(err)
+    return res.status(400).send({
+      errors: [{
+        code: 'Unknow',
+        message: err.toString()
+      }]
+    })
   }
 }
 
